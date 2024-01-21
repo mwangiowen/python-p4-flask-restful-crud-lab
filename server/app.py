6889b73bf38c51a96fe776b7a3802d55f2f086c9
mwangiowen <owen31588@gmail.com>
2024-01-21 14:55:29 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -7,9 +5,9 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -18,7 +16,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -27,9 +24,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,18 +35,38 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        data = request.get_json()
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response("", 204)
 
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
